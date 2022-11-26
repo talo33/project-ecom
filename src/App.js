e16b385df26551a85f57f17014f13f97d0d3c1d9
talo33 <114129828+talo33@users.noreply.github.com>
2022-11-27 04:03:38 +0700

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Pages.js/Home'
+// import About from './Pages.js/About'
+
+
+const App = () => {
+  return (
+    <Home/>
+    // <About/>
+  )
+}
+
+export default App
---
+++ src/Pages.js/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SliderforAbout from '../Components/SliderforAbout'
+import AnnouncementForAbout from '../Components/AnnouncementForAbout'
+
+const About = () => {
+  return (
+    <div>
+        <AnnouncementForAbout/>
+        <SliderforAbout/>
+    </div>
+  )
+}
+
+export default About
